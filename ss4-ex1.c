384ba6dae8d0e5948756ccaab18fa701993a0fcc
BMVu2403 <buiminhvu06hn@gmail.com>
2024-11-23 19:05:16 +0700

    Add files via upload

---
+++ ss4-ex1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    printf("Nhap vao mot so nguyen: ");
+    scanf("%d", &number);
+
+    if (number > 0) {
+        printf("So %d la so duong.\n", number);
+    } else if (number < 0) {
+        printf("So %d là so am.\n", number);
+    } else {
+        printf("So vua nhap la so 0.\n");
+    }
+
+    return 0;
+}
+
